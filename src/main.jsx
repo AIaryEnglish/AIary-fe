c7642230cd82af96a53fdf1883e488509d26f15f
Sey <ppresi14789@gmail.com>
2025-08-22 13:10:27 +0900

    fix: fontawsome 파일삭제

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-import "@fortawesome/fontawesome-free/js/all.js";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
