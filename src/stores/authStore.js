51fcd16d68a32f9ef4d0b9aca4d70565e9848bf6
Sara han <hans63237@gmail.com>
2025-08-23 19:26:18 +0900

    refactor: 유저정보 스토어에만 저장하도록 변경

--- src/stores/authStore.js
+++ src/stores/authStore.js
@@ -1,22 +1,18 @@
+// stores/useAuthStore.js
 import { create } from "zustand";
 
 export const useAuthStore = create((set, get) => ({
   token: sessionStorage.getItem("token") || null,
-  user: JSON.parse(sessionStorage.getItem("user") || "null"),
+  user: null, // ← 유저는 메모리만
 
   login: ({ token, user }) => {
-    // 세션스토리지 저장
+    // 토큰만 스토리지에
     sessionStorage.setItem("token", token);
-    sessionStorage.setItem("user", JSON.stringify(user));
-
-    set({ token, user });
+    set({ token, user: user ?? null });
   },
 
   logout: () => {
-    // 세션스토리지 제거
     sessionStorage.removeItem("token");
-    sessionStorage.removeItem("user");
-
     set({ token: null, user: null });
   },
 
