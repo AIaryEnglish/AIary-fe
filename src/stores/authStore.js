fbf7985afdadcbe34cd31638f134c1d1f91de8af
Sara han <hans63237@gmail.com>
2025-08-23 19:56:25 +0900

    chore: 불필요한 주석제거

--- src/stores/authStore.js
+++ src/stores/authStore.js
@@ -1,12 +1,10 @@
-// stores/useAuthStore.js
 import { create } from "zustand";
 
 export const useAuthStore = create((set, get) => ({
   token: sessionStorage.getItem("token") || null,
-  user: null, // ← 유저는 메모리만
+  user: null,
 
   login: ({ token, user }) => {
-    // 토큰만 스토리지에
     sessionStorage.setItem("token", token);
     set({ token, user: user ?? null });
   },
