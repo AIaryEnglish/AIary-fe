7f12bcfe492cf8adf3641a17db1502278fe831da
Hyeonjin <oboki2001@naver.com>
2025-08-25 18:04:21 +0900

    Feat: useDiaryStore 확장 (월별/일별 일기 상태 관리)

--- src/stores/useDiaryStore.js
+++ src/stores/useDiaryStore.js
@@ -4,11 +4,25 @@ import dayjs from "dayjs";
 const useDiaryStore = create((set) => ({
   selectedDate: dayjs(),
   diaries: [],
+  daysMap: {}, // { "YYYY-MM-DD": { id, canWrite } }
+  diariesByDate: {}, // { "YYYY-MM-DD": diaryDoc }
 
   setSelectedDate: (date) => set({ selectedDate: date }),
   addDiary: (date, title, content, image, isPublic) =>
     set((state) => ({
       diaries: [...state.diaries, { date, title, content, image, isPublic }],
+    })),
+  setMonthDays: (days) =>
+    set(() => {
+      const map = {};
+      for (const d of days) {
+        map[d.date] = { id: d.id, canWrite: d.canWrite };
+      }
+      return { daysMap: map };
+    }),
+  setDiaryForDate: (dateKey, diary) =>
+    set((state) => ({
+      diariesByDate: { ...state.diariesByDate, [dateKey]: diary },
     })),
 }));
 
