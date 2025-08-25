7c1d1b8bbbd8071a445b29ed6e52430c77f43b5e
Seunga Jeong <jsa3341@gmail.com>
2025-08-25 23:32:28 +0900

    Feat: QueryClient 다시 불러오기

--- src/hooks/useCreateDiary.js
+++ src/hooks/useCreateDiary.js
@@ -1,15 +1,19 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { createDiaryApi } from "../apis/diaryApi";
 import useDiaryStore from "../stores/useDiaryStore";
 
 export const useCreateDiary = () => {
   const { setDiaries } = useDiaryStore();
+  const queryClient = useQueryClient();
+
   const mutation = useMutation({
     mutationFn: (diary) => createDiaryApi(diary),
     onSuccess: (response) => {
       if (response.success) {
         setDiaries(response.data);
         alert("일기 생성 완료!");
+        queryClient.invalidateQueries({queryKey:["myDailyDiary"]});
+        queryClient.invalidateQueries({queryKey:["diaries"]});
       }
     },
     onError: (error) => {
