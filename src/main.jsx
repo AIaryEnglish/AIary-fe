50cd8c95bb10b6cacb5c0fa565192483395bc003
Sara han <hans63237@gmail.com>
2025-08-29 10:03:33 +0900

    feat: 전역 폰트 테마 추가

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,9 @@ import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { GoogleOAuthProvider } from "@react-oauth/google";
+import { ThemeProvider } from "@mui/material/styles";
+import { theme } from "./theme";
+import { CssBaseline } from "@mui/material";
 const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 // QueryClient 인스턴스 생성
 const queryClient = new QueryClient({
@@ -21,12 +24,15 @@ const queryClient = new QueryClient({
 });
 
 createRoot(document.getElementById("root")).render(
-  <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <App />
-        <ReactQueryDevtools initialIsOpen={false} />
-      </BrowserRouter>
-    </QueryClientProvider>
-  </GoogleOAuthProvider>
+  <ThemeProvider theme={theme}>
+    <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+      <QueryClientProvider client={queryClient}>
+        <CssBaseline />
+        <BrowserRouter>
+          <App />
+          <ReactQueryDevtools initialIsOpen={false} />
+        </BrowserRouter>
+      </QueryClientProvider>
+    </GoogleOAuthProvider>
+  </ThemeProvider>
 );
